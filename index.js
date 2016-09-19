d5f5633c11ed658c30eaf62625282f63a4de603d
Shmavon Gazanchyan <sgazanchyan@gmail.com>
2016-09-19 11:37:25 +0100

    Update to 0.11.0

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 var path = require('path');
 var ghost = require('ghost');
 
-// temporary fix for https://github.com/TryGhost/Ghost/issues/7336
-require('ghost/core/server/overrides');
-
 ghost({
     config: path.join(__dirname, 'config.js')
 }).then(function (ghostServer) {
